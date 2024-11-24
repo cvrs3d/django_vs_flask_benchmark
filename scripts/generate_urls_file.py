3c0f7b97818793b4ffe98a2234a089107fcc13dd
Yevgeniy Ustinov <ev.ustinov03@gmail.com>
2024-11-24 17:11:28 +0100

    Created script for generating url pool for benchmarking

---
+++ scripts/generate_urls_file.py
@@ -0,0 +1,14 @@
+def generate_order_urls(base_url, num_records, output_file):
+    with open(output_file, 'w') as file:
+        for i in range(1, num_records + 1):
+            url = f"{base_url}/api/orders/{i}/\n"
+            file.write(url)
+
+
+if __name__ == "__main__":
+    BASE_URL = "http://localhost:8000"
+    NUM_RECORDS = 1000000
+    OUTPUT_FILE = "order_urls.txt"
+
+    generate_order_urls(BASE_URL, NUM_RECORDS, OUTPUT_FILE)
+    print(f"Все URL-адреса успешно записаны в {OUTPUT_FILE}")
