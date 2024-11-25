73ec55e9dbe4ac0a0d25ec10f00e63011ceb5569
Yevgeniy Ustinov <ev.ustinov03@gmail.com>
2024-11-25 12:18:09 +0100

    Testing with ORM queries on Django

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from testing.views import OrderViewSet
+# from rest_framework.routers import DefaultRouter
+from testing.views import OrderView
 
-router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
+# router = DefaultRouter()
+# router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    # path('api/', include(router.urls)),
+    path('api/orders/<int:pk>/', OrderView.as_view(),)
 ]
--- django_app/testing/views.py
+++ django_app/testing/views.py
@@ -1,8 +1,39 @@
+from itertools import product
+
 from rest_framework import viewsets
-from .models import Order
 from .serializers import OrderSerializer
+from django.http import JsonResponse
+from django.views import View
+from django.core import serializers
+from .models import Order, Customer, Product
 
+# class OrderViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
 
-class OrderViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+class OrderView(View):
+    def get(self, request, pk):
+        order = Order.objects.select_related(
+            'customer'
+        ).prefetch_related('products').get(id=pk)
+
+        data = {
+            'order': {
+                'id': order.id,
+                'created_at': order.created_at,
+                'customer': {
+                    'id': order.customer.id,
+                    'name': order.customer.name,
+                    'email': order.customer.email,
+                },
+                'products': [
+                    {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                    } for product in order.products.all()
+                ]
+            },
+        }
+
+        return JsonResponse(data)
